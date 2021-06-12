9fdc84a63a0adb9805cf66423cc5f23a7d8293c1
Victor Isidoro <victorisidoro@icloud.com>
2021-06-12 14:31:16 +0100

    Full participants and ignore list updated to ignore the update of avatars

---
+++ script.js
@@ -0,0 +1,26 @@
+// RANDOMIZE PRESENTERS
+// Objective: Pick 3 presenters at random from a list and give back the result
+
+// Define all presenters
+const allPresenters = ['Alexej', 'Guilherme', 'Lorianne', 'Natalie', 'Pallavi', 'Peter', 'Pilar', 'Salvatore', 'Sasmitha', 'Victor']
+
+// Keep track of past presenters - missing the type of presentation (Kahoot/TechWatch)
+const pastPresenters = ['Victor', 'Salvatore', 'Alexej']
+
+// Create an array to keep the new presenters for next week
+let newPresenters = []
+
+// Function to get 3 random presenters, excluding the past presenters
+
+// Event Listener for the button to call the randomize funtion
+
+// Append all registered presenters to the page
+const finalResult = document.getElementById('presenters-list')
+// cycle through the presenters and create a div for each participant and append it
+
+
+// Append the past presenters to the page
+
+// Apply the style to the new picks
+
+// Apply the style to the last 3 past presenters to avoid being picked
